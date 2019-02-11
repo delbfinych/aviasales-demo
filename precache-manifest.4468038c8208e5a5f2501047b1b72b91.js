8eed8aff2b5923c653204eadec868ccf4ede2d32
Villione <dminario@gmail.com>
2019-02-11 23:13:17 +0300

    Updates

---
+++ precache-manifest.4468038c8208e5a5f2501047b1b72b91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f1526991db872ba7eb55aafebc481dd",
+    "url": "/aviasales-demo/static/media/Logo.3f152699.svg"
+  },
+  {
+    "revision": "8cfc7d46676adf040f5a",
+    "url": "/aviasales-demo/static/js/runtime~main.8cfc7d46.js"
+  },
+  {
+    "revision": "f9f36d6c7ab07e3837a2",
+    "url": "/aviasales-demo/static/js/main.f9f36d6c.chunk.js"
+  },
+  {
+    "revision": "e1cbbbe68829fdcbe3ab",
+    "url": "/aviasales-demo/static/js/1.e1cbbbe6.chunk.js"
+  },
+  {
+    "revision": "f9f36d6c7ab07e3837a2",
+    "url": "/aviasales-demo/static/css/main.a46e6f91.chunk.css"
+  },
+  {
+    "revision": "af57e55f706186575255f0cd250ef1b0",
+    "url": "/aviasales-demo/index.html"
+  }
+];
